803e1170ed8c1b423b40e9b249591c32c0ba1870
Pat McClernan <patmccler@gmail.com>
2020-05-06 15:25:26 -0400

    finds anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize word
+    self.word = word
+  end
+
+  def match(words)
+    words.select { |word| anagram_of? word }
+  end
+
+  def anagram_of?(test_word)
+    test_word.split("").sort == @word.split("").sort
+  end
+end
