34d1f8ff7c060fad0a1e0ce1757f4ff47f97b399
Liam Tinney <ltinney@powerhrg.com>
2020-05-06 16:43:35 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :scrutinize
+
+  def initialize(scrutinize)
+    @scrutinize = scrutinize
+  end
+
+  def match(array_of_words)
+    array_of_words.select do |jawn|
+      (@scrutinize.split("").sort) == (jawn.split("").sort)
+    end
+  end
+end
